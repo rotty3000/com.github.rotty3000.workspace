52c5203b1bcb33004a84da4ff5f732591c6cb869
Raymond Auge <raymond.auge@liferay.com>
2021-03-21 22:45:51 -0400

    The generated class
    Signed-off-by: Raymond Auge <raymond.auge@liferay.com>

--- modules/adding-javascript-build/src/main/java/adding/javascript/build/AddingJavascriptBuild.java
+++ modules/adding-javascript-build/src/main/java/adding/javascript/build/AddingJavascriptBuild.java
@@ -1,11 +1,27 @@
 package adding.javascript.build;
 import java.util.function.Supplier;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 @Component(
 	immediate = true,
 	property = {
+		"osgi.command.scope=ajb",
+		"osgi.command.function=get"
 	},
 	service = Supplier.class
 )
 public class AddingJavascriptBuild implements Supplier<String> {
+	@Override
+	public String get() {
+		return "Something interesting!";
+	}
+	@Activate
+	void activate() {
+		System.out.println("Activated!");
+	}
+	@Deactivate
+	void deactivate() {
+		System.out.println("Deactivated!");
+	}
 }
